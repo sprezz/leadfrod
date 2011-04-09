d588ad01619d969f11ebce9b0e5cf475cb9f78c5
Max Yurlov <maxyurloff@gmail.com>
2011-04-09 16:07:32 +0300

    Create a dashboard - Jquery

--- rotator/views.py
+++ rotator/views.py
@@ -8,7 +8,7 @@ from django.template import RequestContext
 from datetime import timedelta, date
 
 import logging
-
+import simplejson
 from models import *
 from rotator.trafficholder import TrafficHolder
 from trafficholder import UnknownOrderException
@@ -231,28 +231,15 @@ def get_lead(worker_id):
 def month_revenue(request, template="month_revenue.html"):
     d = date.today() - timedelta(days=31)
     totals = []
-    months = []
-    max = 1
+    days = []
     for i in range(0, 30):
         d += timedelta(days=1)
         total = Earnings.objects.filter(date__day=d.day, date__month=d.month, date__year=d.year).extra(
             select={'total': 'sum(revenue)'})[0].total            
-        if max < total:
-            max = total
-        totals.append(total if total else 0)
-        m = d.strftime('%B')
-        if m not in months:
-            months.append(m)                 
-    """
-    k = 100/max
-    print k
-    for t in totals:
-        t = 100/max*t
-        print t
-    """ 
-    return render_to_response(template, 
-        {'chart': "https://chart.googleapis.com/chart?cht=lc&chs=800x375&chd=t:%s&chxt=x,y&chxl=0:|%s|1:||%d" % \
-            (','.join(str(100/max*n) for n in totals), '|'.join(str(n) for n in months), max) },
+        totals.append(float(total) if total else 0)
+        days.append(d.strftime("%b'%d") if len(days) % 2 == 0 else " ")   
+
+    return render_to_response(template, { 'datax': simplejson.dumps(totals), 'datay': simplejson.dumps(days) },
         context_instance=RequestContext(request))
 
 
