eeae76027cb4ec9c6fd99327cdd083a5760aef7d
Serj Zavadsky <fevral13@gmail.com>
2013-04-26 01:16:25 +0300

    fixes

--- rotator/models/csv_file.py
+++ rotator/models/csv_file.py
@@ -20,6 +20,7 @@ class CSVFile(models.Model):
     #   revenue = models.FloatField(default = 0) to be calculated
     #   percent_completed = models.FloatField(default = 0) to be calculated
     workers = models.ManyToManyField('auth.User', null=True, blank=True,
+                                     symmetrical=True,
                                      related_name='assignments')
     max_offers = models.FloatField(default=1)
     csv_headers = models.TextField(null=False, blank=True)
